5f6537d59c6821c050c5d709a4d56a9147e9d0f0
Roby Hartono <roby.hartono@empulse.de>
2025-02-07 11:20:08 +0100

    test: implemented github action flow for automated testing cypress, with videos artifcat output

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,26 @@ export default defineConfig({
   e2e: {
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.ts',
+    // Enable video recording for test runs
+    video: true,
+
+    // Enable screenshots on test failures
+    screenshotOnRunFailure: true,
+
+    // // Set the reporter to mochawesome for detailed reports
+    // reporter: 'mochawesome',
+
+    // // Configure mochawesome reporter options
+    // reporterOptions: {
+    //   charts: true,
+    //   reportDir: 'cypress/results/reports',
+    //   overwrite: false,
+    //   html: true,
+    //   json: true
+    // }
   },
+
+  // Optionally, configure global settings (like videos/screenshots)
+  videoCompression: 32,
+  trashAssetsBeforeRuns: true
 });
